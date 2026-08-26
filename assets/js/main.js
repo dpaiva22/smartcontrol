7c7413554421d26cb1590f2b737c21946a0bcc3c
EJK6NL109 <ejk6nl109@edusesimg.com.br>
2026-08-26 16:23:32 -0300

    CE SABE

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let nomeUsuario = "Davi";
+let sobreNomeUsuario = "Paiva";
+let data = new Date().toLocaleString("pt-BR",{dateStyle: "full", timeStyle: "short"});
+
+console.log(`Olá, ${nomeUsuario} ${sobreNomeUsuario}! Hoje é ${data}.`);
+
+
+
+
+
