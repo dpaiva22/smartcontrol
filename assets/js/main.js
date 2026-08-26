a8826eb65c21d6a3a1e9a090161ac04f661fcfa5
EJK6NL109 <ejk6nl109@edusesimg.com.br>
2026-08-26 17:10:47 -0300

    CE SABe

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,24 @@
-let nomeUsuario = "Davi";
-let sobreNomeUsuario = "Paiva";
+let nomeUsuario = prompt("Digite seu nome:");
+let sobreNomeUsuario = prompt("Digite seu sobrenome:");
 let data = new Date().toLocaleString("pt-BR",{dateStyle: "full", timeStyle: "short"});
 
+let dia = data.getdate();
+const semana = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"];
+let mes = data.getMonth();
+let horas = data.getHours();
+let minutos = data.getMinutes();
+
+let datahoje = `${semana[dia]}, ${dia}/${mes}/${data.getFullYear()} - ${horas}:${minutos}`;
+
 console.log(`Olá, ${nomeUsuario} ${sobreNomeUsuario}! Hoje é ${data}.`);
+
+setInterval(() => {
+    let bemVindoElement = document.getElementById("bemvindo");
+    if (bemVindoElement) {
+        bemVindoElement.innerHTML = `Bem-vindo, ${nomeUsuario} ${sobreNomeUsuario}! Hoje é ${data}`;
+    }
+}, 3000);
+
 
 
 
