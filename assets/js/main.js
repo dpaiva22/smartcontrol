f31ce72174768cd78e4727a4fa127256bb56faa1
EJK6NL109 <ejk6nl109@edusesimg.com.br>
2026-08-27 16:42:28 -0300

    cesabe

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,21 @@
 let nomeUsuario = prompt("Digite seu nome:");
 let sobreNomeUsuario = prompt("Digite seu sobrenome:");
-let data = new Date().toLocaleString("pt-BR",{dateStyle: "full", timeStyle: "short"});
-
-let dia = data.getdate();
-const semana = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado"];
-let mes = data.getMonth();
-let horas = data.getHours();
-let minutos = data.getMinutes();
-
-let datahoje = `${semana[dia]}, ${dia}/${mes}/${data.getFullYear()} - ${horas}:${minutos}`;
+let data = new Date().toLocaleString("pt-BR",{dateStyle: "full", timeStyle: "short", hour12: false, timeZone: "America/Sao_Paulo"});
 
 console.log(`Olá, ${nomeUsuario} ${sobreNomeUsuario}! Hoje é ${data}.`);
 
-setInterval(() => {
-    let bemVindoElement = document.getElementById("bemvindo");
-    if (bemVindoElement) {
-        bemVindoElement.innerHTML = `Bem-vindo, ${nomeUsuario} ${sobreNomeUsuario}! Hoje é ${data}`;
-    }
-}, 3000);
+const semana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+let dataAtual = new Date();
+const fuso_horario = dataAtual.getTimezoneOffset() / -60;
+let horas = new Date().toLocaleString("pt-BR", { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false });
 
+
+
+setInterval(() => {
+    let bemVindo = document.getElementById("bemvindo");
+    bemVindo.innerHTML = `Olá, ${nomeUsuario} ${sobreNomeUsuario}! Hoje é ${semana[new Date().getDay()]}, ${data} - ${horas} (-${fuso_horario}:00h)`;
+
+}, 3*1000)
 
 
 
